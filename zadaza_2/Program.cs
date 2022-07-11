14392fa0e9f722d154c62a70d6bd0a06f33cfbb3
stas9025 <berezovskiy1991@yandex.ru>
2022-07-11 11:10:55 +0300

    zadaza_2

--- zadaza_2/Program.cs
+++ zadaza_2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пожалуйста три числа!");
+
+Console.Write("Число а = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Число c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c)
+{
+  Console.WriteLine($"max = {a}");
+}
+else if (b > a && b > c)
+{
+  Console.WriteLine($"max = {b}");
+}
+else
+{
+  Console.WriteLine($"max = {c}");
+}
