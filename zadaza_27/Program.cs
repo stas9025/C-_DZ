f47f484b87a4e028a193f52b616f032500cb4999
stas9025 <berezovskiy1991@yandex.ru>
2022-08-09 15:35:31 +0300

    zadaza_27 new

--- zadaza_27/Program.cs
+++ zadaza_27/Program.cs
@@ -7,11 +7,18 @@
 
 Console.WriteLine("Введите пожалуйста число!");
 int a = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
+
+int Sum(int number)
+{
+  int sum = 0;
 
   while(a != 0)
   {
     sum += a % 10;
     a = a / 10; 
   }
-Console.WriteLine($"-> {sum}");
+  return sum;
+
+}
+int res = Sum(a);
+Console.WriteLine($"Сумма всех чисел = {res}");
