7281c58861a10da90cf17f72b944615c6112fce0
stas9025 <berezovskiy1991@yandex.ru>
2022-08-09 15:41:39 +0300

    zadaza_25 new

--- zadaza_25/Program.cs
+++ zadaza_25/Program.cs
@@ -10,11 +10,16 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите пожалуйста степень B!");
 int b = Convert.ToInt32(Console.ReadLine());
 
-int rez = 1;
-
-for (int i = 0; i < b; i++)
+int Step(int number)
 {
-  rez = rez * a;
+  int rez = 1;
+
+  for (int i = 0; i < number; i++)
+  {
+    rez = rez * a;
+  }
+  
+  return rez;
 }
-Console.Write($"-> {rez}");
-return 0;
+int result = Step(b);
+Console.Write($"-> {result}");
