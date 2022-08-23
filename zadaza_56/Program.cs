a36191ab44c0cab21852df10b33b8e9e0da090fd
stas9025 <berezovskiy1991@yandex.ru>
2022-08-23 20:53:33 +0300

    zadaza_56_new

--- zadaza_56/Program.cs
+++ zadaza_56/Program.cs
@@ -61,17 +61,4 @@ void MinSum(int[,] array)
   Console.ReadKey();
 }
 
-
-/*void PrintMatrix(int[,] array)
-{
-  for (int i = 0; i < arr.GetLength(0); i++)
-  {
-    for (int j = 0; j < arr.GetLength(1); j++)
-    {
-      Console.Write(arr[i, j] + " ");
-    }
-    Console.WriteLine();
-  }
-}*/
 MinSum(arr);
-//PrintMatrix(arr);
