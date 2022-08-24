a27d1da7f5eafbbf1440a714c2d1b09b0a726a74
stas9025 <berezovskiy1991@yandex.ru>
2022-08-24 21:55:58 +0300

    zadaza_64

---
+++ zadaza_64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите число N ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+static void Recursion(int n)
+{
+  if (n > 0)
+  {
+    Console.Write("{0} ,", n);
+    Recursion(n - 1);
+  }
+}
+Recursion(n);
